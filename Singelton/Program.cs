5ce91081f5336d5838d98034f6fdcddc937b7252
YaronZandberg <52877527+YaronZandberg@users.noreply.github.com>
2019-07-14 11:47:37 +0300

    Singleton Practice

---
+++ Singelton/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singelton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton s1 = Singleton.GetInstance();
+            Singleton s2 = Singleton.GetInstance();
+            Singleton s3 = Singleton.GetInstance();
+
+            s1.getDate();
+            s2.getDate();
+            s3.getDate();
+        }
+    }
+}
---
+++ Singelton/Singelton.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singelton
+{
+    class Singleton
+    {
+        private static Singleton INSTANCE;
+
+        private static object key = new object();
+        private Singleton()
+        {
+
+        }
+        public void getDate()
+        {
+            Console.WriteLine(DateTime.Now);
+        }
+
+        public static Singleton GetInstance()
+        {
+            if (INSTANCE != null)
+            {
+                return INSTANCE;
+            }
+
+            lock (key)
+            {
+                if (INSTANCE == null)
+                {
+                    INSTANCE = new Singleton();
+                }
+            }
+
+            return INSTANCE;
+        }        
+    }
+}
