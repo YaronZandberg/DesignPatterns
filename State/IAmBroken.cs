c02fdac8f6cf6d3484881f9124b9a1787428fd64
YaronZandberg <52877527+YaronZandberg@users.noreply.github.com>
2019-07-14 15:41:23 +0300

    State Simple

---
+++ State/IAmBroken.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace State
+{
+    class IAmBroken : ILightState
+    {
+        public ILightState Fix()
+        {
+            Console.WriteLine("The lightswitch is off...");
+            return new IAmOff();
+        }
+
+        public ILightState TurnOff()
+        {
+            return this;
+        }
+
+        public ILightState TurnOn()
+        {
+            return this;
+        }
+    }
+}
---
+++ State/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace State
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Context ctx = new Context(new IAmOn());
+            ctx.pressedOff();
+            ctx.pressedOn();
+            ctx.pressedOff();
+            ctx.pressedOff();
+            ctx.pressedOn();
+            ctx.pressedOn();
+            ctx.pressedFix();
+            ctx.pressedFix();
+            ctx.pressedOff();
+            ctx.pressedOn();
+            ctx.pressedFix();
+        }
+    }
+}
