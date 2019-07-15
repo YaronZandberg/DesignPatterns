fd023e0fd2673b038c24002c15997b81af241cf3
YaronZandberg <52877527+YaronZandberg@users.noreply.github.com>
2019-07-15 14:19:14 +0300

    CompositeRecursive - IsBinary

---
+++ CompositeRecursive/Composite.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Composite
+{
+    public class Composite : Component
+    {
+
+        IList<Component> childs = new List<Component>();
+
+        public Composite(string name) : base(name)
+        {
+        }
+
+        public override void AddChild(Component c)
+        {
+            childs.Add(c);
+        }
+
+        public override IList<Component> GetChilds()
+        {
+            return childs;
+        }
+
+        public override void RemoveChild(Component c)
+        {
+            childs.Remove(c);
+        }
+
+        public override void Draw(string space)
+        {
+            Console.WriteLine(space + this.name);
+            //foreach (Component c in childs)
+            //{
+            //    c.Draw(space + "    ");
+            //}
+        }
+
+        public override bool IsBinary()
+        {
+            //if (this.GetChilds().Count() == 0)
+            //{
+            //    return true;
+            //}
+            //else if (this.GetChilds().Count() != 2)
+            //{
+            //    return false;
+            //}
+            //else
+            //{
+            //    return (this.GetChilds().First().IsBinary()) && (this.GetChilds().Last().IsBinary());
+            //}
+
+
+            return ((this.GetChilds().Count() == 0) || (this.GetChilds().Count() == 2));
+        }
+    }
+}
---
+++ CompositeRecursive/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Composite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Component root = new Composite("Canvas root");
+            Component circle1 = new Leaf("Circle 1");
+            Component rectangle = new Leaf("Rectangle x");
+            root.AddChild(circle1);
+            root.AddChild(rectangle);
+
+            Component container1 = new Composite("Container 1");
+            Component circle2 = new Leaf("Circle 1");
+            Component circle3 = new Leaf("Circle 2");
+            container1.AddChild(circle2);
+            container1.AddChild(circle3);
+
+            root.AddChild(container1);
+
+            Component container2 = new Composite("Container of triangles");
+            Component t1 = new Leaf("Triangle 1");
+            Component t2 = new Leaf("Triangle 2");
+            container2.AddChild(t1);
+            container2.AddChild(t2);
+
+            root.AddChild(container2);
+
+            Draw(root, "");
+
+            Console.WriteLine("Answer is: " + IsBinary(root));
+        }
+
+        static void Draw(Component c, string space)
+        {
+            c.Draw(space);
+
+            if (c.GetChilds() == null)
+                return;
+
+            foreach(Component child in c.GetChilds())
+            {
+                Draw(child, space + "    ");
+            }
+        }
+
+        static bool IsBinary(Component biNonde)
+        {
+            // Check if there is a leaf
+            if (biNonde.GetChilds() == null)
+            {
+                return true;
+            }
+            // If we get here it isn't a leaf it is a composite
+            // Check if the current composite isn't binary
+            else if (!biNonde.IsBinary())
+            {
+                return false;
+            }
+            // Perform the method on every child
+            else
+            {
+                return ((biNonde.GetChilds().First().IsBinary()) && biNonde.GetChilds().Last().IsBinary());
+            }
+        }
+    }
+}
