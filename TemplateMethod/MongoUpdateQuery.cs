1f2b3664668e2e0dc33aa7bfcb70ff57db525b23
YaronZandberg <52877527+YaronZandberg@users.noreply.github.com>
2019-07-16 15:39:36 +0300

    TemplateMethod - DB Connection

---
+++ TemplateMethod/MongoUpdateQuery.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemplateMethod
+{
+    public class MongoUpdateQuery : MongoTemplateBase
+    {
+        protected override void ExecuteQuery()
+        {
+            Console.WriteLine("UPDATE DB_Table SET column1 = value1");
+        }
+    }
+}
---
+++ TemplateMethod/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemplateMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //new OracleSelectAllQuery().Run();
+            OracleTemplateBase selectAllQuery = new OracleSelectAllQuery();
+            selectAllQuery.Run();
+            Console.WriteLine();
+
+            //new OracleUpdateQuery().Run();
+            OracleTemplateBase updateOracleQuery = new OracleUpdateQuery();
+            updateOracleQuery.Run();
+            Console.WriteLine();
+
+            MongoTemplateBase insertMongoQuery = new MongoInsertQuery();
+            insertMongoQuery.Run();
+            Console.WriteLine();
+
+            MongoTemplateBase updateMongoQuery = new MongoUpdateQuery();
+            updateMongoQuery.Run();
+            Console.WriteLine();
+
+            MongoTemplateBase deleteMongoQuery = new MongoDeleteQuery();
+            deleteMongoQuery.Run();
+            Console.WriteLine();
+        }
+    }
+}
