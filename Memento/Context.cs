02a0be2fde69a85742b04e240c7f3ba661416baf
YaronZandberg <52877527+YaronZandberg@users.noreply.github.com>
2019-07-16 15:48:37 +0300

    Memento - Undo&Redo

---
+++ Memento/Context.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Memento
+{
+    public class Context
+    {
+
+        TextCareTaker careTake = new TextCareTaker();
+
+        public void Save(RichTextBox richTex)
+        {
+            careTake.AddMemento(new TextMemento(richTex.Text));
+        }
+
+        public void LoadPrev(RichTextBox richText)
+        {
+            TextMemento prevMemento = careTake.GetPrev();
+            if (prevMemento != null && prevMemento.savedText != null)
+            {
+                richText.Text = prevMemento.savedText;
+            }
+
+        }
+
+        public void LoadNext(RichTextBox richText)
+        {
+            TextMemento nextMemento = careTake.GetNext();
+            if (nextMemento != null && nextMemento.savedText != null)
+            {
+                richText.Text = nextMemento.savedText;
+            }
+        }
+    }
+}
---
+++ Memento/TextCareTaker.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Memento
+{
+    class TextCareTaker
+    {
+        List<TextMemento> mementoList = new List<TextMemento>();
+        int index = -1;
+
+        public void AddMemento(TextMemento memento)
+        {
+            this.mementoList.Add(memento);
+            this.index++;
+            mementoList.RemoveRange(index, (mementoList.Count - index - 1));
+        }
+
+        public TextMemento GetPrev()
+        {
+            if (index != -1 && index < this.mementoList.Count())
+            {
+                this.index--;
+                return this.mementoList[index + 1];
+            }
+
+            this.index = 0;
+            return null;
+        }
+
+        public TextMemento GetNext()
+        {
+            if (index != -1 && index + 1 < this.mementoList.Count())
+            {
+                this.index++;
+                return this.mementoList[index];
+            }
+            else if (index == -1)
+            {
+                index = 0;
+            }
+
+            return null;
+        }
+    }
+}
