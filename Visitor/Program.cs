92e38aea178c934ca53acc43f9761946b9c65281
YaronZandberg <52877527+YaronZandberg@users.noreply.github.com>
2019-07-16 10:14:18 +0300

    Visitor - Products_Sales

---
+++ Visitor/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Visitor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Liquor vodka = new Liquor(95);
+            Perfume perf = new Perfume(100);
+            ShopRent shop = new ShopRent(10000);
+
+            Console.WriteLine(vodka);
+            Console.WriteLine(perf);
+            Console.WriteLine(shop);
+
+            IVisitor endOfYearVisitor = new EndOfYearTax();
+            IVisitor oneShekelSaleVisitor = new OneShekelSaleTax();
+
+            Console.WriteLine($"Price after tax = {vodka.Accept(endOfYearVisitor)}");
+            Console.WriteLine($"Price after tax = {perf.Accept(endOfYearVisitor)}");
+            Console.WriteLine($"One shekel sale = {vodka.Accept(oneShekelSaleVisitor)}");
+            Console.WriteLine($"One shekel sale = {perf.Accept(oneShekelSaleVisitor)}");
+            Console.WriteLine($"Price after tax = {shop.Accept(endOfYearVisitor)}");
+            Console.WriteLine($"One shekel sale = {shop.Accept(oneShekelSaleVisitor)}");
+        }
+    }
+}
---
+++ Visitor/ShopRent.cs
@@ -0,0 +1,22 @@
+﻿namespace Visitor
+{
+    public class ShopRent : IVisitable
+    {
+        public double Price { get; set; }
+
+        public ShopRent(double price)
+        {
+            this.Price = price;
+        }
+
+        public double Accept(IVisitor visitor)
+        {
+            return visitor.Visit(this);
+        }
+
+        public override string ToString()
+        {
+            return $"Shop price : {Price}";
+        }
+    }
+}
